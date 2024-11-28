b163f4005630d5a0536db52324e135dc1b246adb
iceguan <iceguan@tencent.com>
2024-11-28 16:20:30 +0800

    增加导出cs代码

---
+++ src/Main.hx
@@ -0,0 +1,13 @@
+import hxDaedalus.data.ConstraintSegment;
+import hxDaedalus.data.ConstraintShape;
+import hxDaedalus.data.Mesh;
+import hxDaedalus.data.Object;
+import hxDaedalus.data.Vertex;
+import hxDaedalus.factories.RectMesh;
+import hxDaedalus.view.SimpleView;
+
+class Main {
+    static function main() {
+        trace("Hello, world!");
+    }
+}
